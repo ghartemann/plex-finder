8384ed4a2c74df41543f996ddb43cd1ff3f05873
ghartemann <g.hartemann@gmail.com>
2022-08-09 11:01:19 +0200

    created services, updated CSS, made things work better

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Watchlist;
 use App\Repository\WatchlistRepository;
+use App\Service\WatchlistService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,6 @@ use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ImportController extends AbstractController
 {
@@ -22,27 +22,18 @@ class ImportController extends AbstractController
      * @throws ClientExceptionInterface
      */
     #[Route('/import', name: 'app_import')]
-    public function index(HttpClientInterface $client, WatchlistRepository $watchlistRepository): Response
+    public function index(
+        WatchlistRepository $watchlistRepository,
+        WatchlistService    $watchlistService
+    ): Response
     {
-        $response = $client->request('GET', 'https://metadata.provider.plex.tv/library/sections/watchlist/all?&includeFields=ratingKey%2Ctitle%2Cyear%2Cduration%2Cthumb%2Crating&includeElements=Guid&sort=watchlistedAt%3Adesc&type=1&X-Plex-Token=zDyYfQtN_jfYqq_JPbiB');
-        $statusCode = $response->getStatusCode();
-
-        $data = [];
-
-        if ($statusCode == "200") {
-            $xmlData = $response->getContent();
-            $xmlData = simplexml_load_string($xmlData);
-            $jsonData = json_encode($xmlData);
-            $data = json_decode($jsonData, true);
-        }
+        // fetching data from service
+        $data = $watchlistService->getWatchlist();
 
         foreach ($data["Video"] as $dataElement) {
             if ($watchlistRepository->findOneBy(['title' => $dataElement['@attributes']['title']]) == null) {
-                $summary = $client->request('GET', 'https://metadata.provider.plex.tv/library/metadata/' . $dataElement['@attributes']['ratingKey'] . '?X-Plex-Token=zDyYfQtN_jfYqq_JPbiB');
-                $xmlData2 = $summary->getContent();
-                $xmlData2 = simplexml_load_string($xmlData2);
-                $jsonData2 = json_encode($xmlData2);
-                $data2 = json_decode($jsonData2, true);
+                // get missing info
+                $missingInfo = $watchlistService->getMissingInfo($dataElement['@attributes']['ratingKey']);
 
                 $watchlist = new Watchlist();
                 $watchlist
@@ -51,11 +42,13 @@ class ImportController extends AbstractController
                     ->setDuration($dataElement['@attributes']['duration'])
                     ->setRating($dataElement['@attributes']['rating'])
                     ->setThumbnail($dataElement['@attributes']['thumb'])
-                    ->setSummary($data2["Video"]['@attributes']['summary']);
+                    ->setSummary($missingInfo["Video"]['@attributes']['summary']);
                 $watchlistRepository->add($watchlist, true);
             }
         }
 
-        return $this->render('import/index.html.twig', ['data' => $data, 'statusCode' => $statusCode]);
+        $movies = $watchlistRepository->findAll();
+
+        return $this->render('import/index.html.twig', ['movies' => $movies]);
     }
 }
---
+++ src/Service/WatchlistService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class WatchlistService
+{
+    // TODO: move this elsewhere ASAP
+    public const TOKEN = 'zDyYfQtN_jfYqq_JPbiB';
+
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getWatchlist(): array
+    {
+        // TODO: not the best way to do this but it's more readable this way
+        $response = $this->client->request(
+            'GET', 'https://metadata.provider.plex.tv/library/sections/watchlist/all?&includeFields=' .
+            'ratingKey%2C' .
+            'title%2C' .
+            'year%2C' .
+            'duration%2C' .
+            'thumb%2C' .
+            'rating' .
+            '&includeElements=Guid&sort=watchlistedAt%3Adesc&type=1&X-Plex-Token=' . self::TOKEN);
+        $statusCode = $response->getStatusCode();
+
+        $data = [];
+
+        if ($statusCode == '200') {
+            $xmlData = $response->getContent();
+            $xmlData = simplexml_load_string($xmlData);
+            $jsonData = json_encode($xmlData);
+            $data = json_decode($jsonData, true);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    // get info missing from the simple watchlist request (summary)
+    public function getMissingInfo($ratingKey): array
+    {
+        $response = $this->client->request('GET', 'https://metadata.provider.plex.tv/library/metadata/' . $ratingKey . '?X-Plex-Token=' . self::TOKEN);
+        $xmlData = $response->getContent();
+        $xmlData = simplexml_load_string($xmlData);
+        $jsonData = json_encode($xmlData);
+        $missingInfo = json_decode($jsonData, true);
+
+        return $missingInfo;
+    }
+}
